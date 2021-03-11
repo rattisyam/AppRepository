7ad8ab478999e9ad343a597673f5c2bc78fde056
rattisyam <syam098@gmail.com>
2021-03-11 16:46:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 5000;
+var port = 3000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
