ab43c1070abdcfe3a1e86c024dbf40ecde87d505
Arun prasad Krishnaraj <34764@Hexaware.com>
2021-04-09 09:38:33 +0530

    updated app version to 2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Current application version is: 1\n');
+  res.send('Current application version is: 2\n');
 });
 
 app.listen(PORT)
