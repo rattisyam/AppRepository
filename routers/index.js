ca4d96044ca339cef18c5977675f71f4c45426e4
Arun prasad Krishnaraj <34764@Hexaware.com>
2021-04-12 16:40:01 +0530

    fixed pug templating for html pages

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var env = process.env.APP_ENV
+var ver = process.env.APP_VER
+
 router.get('/', function(req, res) {
-  res.render('index', {
-  })
+  res.render('index', {'env': env ,'ver':ver})
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 
 var app = express();
 
+
+
 app.set('view engine','pug');
 app.set('views','./views');
 
